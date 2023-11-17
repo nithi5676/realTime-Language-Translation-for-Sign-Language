69caf7b3d479f12ce49049158308d08fa118d499
mdisrar05 <mohammed.israr636@gmail.com>
2023-11-17 21:38:52 +0530

    initial commit

---
+++ collect_imgs.py
@@ -0,0 +1,46 @@
+import os
+import cv2
+
+DATA_DIR = './data'
+if not os.path.exists(DATA_DIR):
+    os.makedirs(DATA_DIR)
+
+number_of_classes = 36  # Update the number of classes to 36 (0 to 35)
+dataset_size = 100
+
+cap = cv2.VideoCapture(0)  # Use camera index 0 (the default camera)
+for j in range(number_of_classes):
+    if not os.path.exists(os.path.join(DATA_DIR, str(j))):
+        os.makedirs(os.path.join(DATA_DIR, str(j)))
+
+    if j < 26:
+        class_label = chr(ord('A') + j)  # Letters from 'A' to 'Z'
+    else:
+        class_label = str(j - 26)  # Numbers from 0 to 9
+
+    print('Collecting data for class {}'.format(class_label))
+
+    done = False
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            continue  # Skip frames without valid data
+        cv2.putText(frame, 'Ready? Press "Q" ! :)', (100, 50), cv2.FONT_HERSHEY_SIMPLEX, 1.3, (0, 255, 0), 3,
+                    cv2.LINE_AA)
+        cv2.imshow('frame', frame)
+        if cv2.waitKey(25) == ord('q'):
+            break
+
+    counter = 0
+    while counter < dataset_size:
+        ret, frame = cap.read()
+        if not ret:
+            continue  # Skip frames without valid data
+        cv2.imshow('frame', frame)
+        cv2.waitKey(25)
+        cv2.imwrite(os.path.join(DATA_DIR, str(j), '{}.jpg'.format(counter)), frame)
+
+        counter += 1
+
+cap.release()
+cv2.destroyAllWindows()
